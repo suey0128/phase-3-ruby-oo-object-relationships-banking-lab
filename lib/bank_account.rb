5397c6534f0d825c1abfa3edabb710636f501f13
suey0128 <syui0128@gmail.com>
2021-07-01 16:33:58 -0700

    commit tree b0bcbb7ed8a83a56bb86170cb999fd2b9f5d829f

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,34 @@
 class BankAccount
+    @@all = []
 
+    def self.all
+        @@all 
+    end
+
+    attr_reader :name
+    attr_accessor :balance, :status
+
+    def initialize name, balance=1000, status="open"
+        @name = name
+        @balance = balance
+        @status = status
+        @@all << self
+    end
+
+    def deposit amount
+        @balance = @balance + amount
+    end
+
+    def display_balance
+        "Your balance is $#{self.balance}."
+    end
+
+    def valid?
+        #open status && balance > 0 
+        self.balance > 0 && self.status == "open" ? true : false
+    end
+
+    def close_account
+        self.status = "closed"
+    end
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,53 @@
 class Transfer
-  # your code here
+  @@all = []
+
+    def self.all
+        @@all 
+    end
+
+    attr_reader :bank_account, :sender, :receiver, :amount
+    attr_accessor :status
+
+    def initialize sender, receiver, status = "pending", amount
+        # @bank_account = bank_account
+        @sender = sender
+        @receiver = receiver
+        @status = status
+        @amount = amount
+        @@all << self
+    end
+
+    def valid?
+      self.sender.valid? && self.receiver.valid? ? true : false 
+    end
+
+    def execute_transaction
+      #if the sender acc has enough balance && acc status is open
+      @execute_transaction ||= transaction_helper
+    end
+
+    def reverse_transfer
+      if self.status == "complete"
+        self.sender.balance = self.sender.balance + self.amount
+        self.receiver.balance = self.receiver.balance - self.amount
+        self.status = "reversed"
+      end
+    end
+
+
+    private 
+
+    def transaction_helper
+      if self.sender.balance > self.amount && self.sender.valid? == true && self.receiver.valid? == true
+        self.sender.balance = self.sender.balance - self.amount
+        self.receiver.balance = self.receiver.balance + self.amount
+        self.status = "complete"
+      else 
+        self.status = "rejected"
+        "Transaction rejected. Please check your account balance."
+      end
+    end
+
+
+
 end
